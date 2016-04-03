4d5e5a9698beb8046f0d4c046c8c3983af5a1466
Mauricio Ezequiel Rodriguez <rodriguez.mauricioe@gmail.com>
2016-04-03 20:09:21 -0300

    Subo el proyecto nuevamente

---
+++ ProyectoUnityVJ/Assets/Scripts/Car/WheelPair.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class WheelPair
+{
+    //public Collider rightWheelCollider, leftWheelCollider;
+    public GameObject rightWheel, leftWheel;
+    public bool motor, steer;
+}
---
+++ ProyectoUnityVJ/Assets/Scripts/K.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class K
+{
+    // ===== LAYERS =====
+    public const int LAYER_GROUND = 8;
+
+    // ===== Ground Check =====
+    public const float IS_GROUNDED_RAYCAST_DISTANCE = 2;
+
+    // ===== JEEP CONFIG =====
+    public const float JEEP_MAX_SPEED = 100;
+    public const float JEEP_MIN_SPEED = 0;
+    public const float JEEP_ACCELERATION_RATE = 10;
+    public const float JEEP_DECELERATION_RATE = 50;
+    public const float JEEP_MAX_STEERING_ANGLE = 30;
+    public const float JEEP_STEER_SPEED = 0.8f;
+    public const float JEEP_BRAKE = 1.5f;
+
+    public const float GRAVITY = 9.8f;
+}
