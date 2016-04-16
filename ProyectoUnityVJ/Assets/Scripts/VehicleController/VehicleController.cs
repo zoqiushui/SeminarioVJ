7e6635ced66df605560ac13a4189326429b7eab0
martinletizia <martin.letiziaa@gmail.com>
2016-04-16 10:19:45 -0300

    Actualización del proyecto.
    Eliminé aquellos archivos que ya no hacían falta y organicé un poco el
    hierarchy.

--- ProyectoUnityVJ/Assets/Scripts/VehicleController/VehicleController.cs
+++ ProyectoUnityVJ/Assets/Scripts/VehicleController/VehicleController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-public class TestController : MonoBehaviour
+public class VehicleController : MonoBehaviour
 {
     public float maxTorque;
     public Transform centerOfMass;
