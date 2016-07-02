601b594f0e750910e0c58cc49d42e799a202df3d
Raykaster <obrasdemarcos@gmail.com>
2016-07-02 02:33:04 -0300

    Tooltips en el HUB

---
+++ ProyectoUnityVJ/Assets/Scripts/Menus and interface/TraitHUB.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TraitHUB : MonoBehaviour
+{
+
+    public string traitDescription;
+    public GameObject toolTip;
+
+    void OnMouseEnter()
+    {
+        toolTip.SetActive(true);
+        toolTip.GetComponent<Tooltip>().textContainer.GetComponent<Text>().text = traitDescription;
+        toolTip.transform.position = new Vector3(transform.position.x, transform.position.y + 10, transform.position.z);
+    }
+
+    void OnMouseExit()
+    {
+        toolTip.SetActive(false);
+        toolTip.GetComponent<Tooltip>().textContainer.GetComponent<Text>().text ="";
+    }
+
+}
