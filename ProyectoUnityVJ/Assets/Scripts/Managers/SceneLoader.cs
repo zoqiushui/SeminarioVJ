9f92c322c6c009137dd366932c8c977005f487ae
Mauricio Ezequiel Rodriguez <rodriguez.mauricioe@gmail.com>
2016-07-03 14:42:18 -0300

    Loader asincronico

---
+++ ProyectoUnityVJ/Assets/Scripts/Managers/SceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public SCENES_NUMBER sceneToLoad;
+    public Text textLoading;
+
+    void Start()
+    {
+        StartCoroutine(Load((int)sceneToLoad));
+    }
+
+    IEnumerator Load(int scene)
+    {
+        yield return null;
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync(scene);
+        ao.allowSceneActivation = false;
+
+        while (!ao.isDone)
+        {
+            float progress = Mathf.Clamp01(ao.progress / 0.9f);
+            if (ao.progress == 0.9f)
+            {
+                ao.allowSceneActivation = true;
+            }
+            textLoading.text = "LOADING\n" + (int)(progress * 100) + "%";
+            yield return null;
+        }
+    }
+}
